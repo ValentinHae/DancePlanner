9a1012b72c87d1558dd1de5645b700ddcfc1e78c
Tobias Kister <TobiasKister20@gmail.com>
2022-02-03 18:34:43 +0100

    Fix of GithubToken Access Privages

--- TelegramBot/bot.ts
+++ TelegramBot/bot.ts
@@ -1,22 +1,13 @@
-// Imports of external Modules
-import {
-    GitRequestConfig,
-    getCurrentFileContent,
-    updateFileContent
-} from "https://deno.land/x/git_hub_file_requests@v0.1.2/mod.ts";
-import {
-    DanceEvent
-} from "./Event.ts";
+import{ GitRequestConfig, getCurrentFileContent, updateFileContent} from "https://deno.land/x/git_hub_file_requests@v0.1.2/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 
-// Constants and Parameter
 const GitHub = new GitRequestConfig("ValentinHae", "DancePlanner", "Events.json")
-
-let content = '{"id": "124124124","title": "FOLK FEST CUBA SI","imageName": "Dancing-Bachata, Salsa, Merengue-in-Ukraine-Kyiv-on-2020-11-20-at-3pm-1595276066817.jpg","dances": "Bachata, Salsa, Merengue","countryCode": "Germany","city": "Berlin","street": "n.a.","housenumber": "n.a.","startDate": "2022-01-30","startTime": "2022-01-21","link": "DancePlanner","lat": 50.45466,"lon": 30.5238}'
+let content = '[{"test":"test"}, "test2":3]'
 
 async function run() {
-    let response = await updateFileContent(GitHub,content) 
-    console.log(response)
-};
-
+    let x = await getCurrentFileContent(GitHub)
+    let y = await updateFileContent(GitHub, content)
+    console.log(`Content in File: ${x}\nStatusCode of Rest-Call: ${y}`)
+}
 run()
