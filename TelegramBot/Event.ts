4b3188cd47ce2d323f0c239c4533df90715a104c
Tobias Kister <TobiasKister20@gmail.com>
2022-02-05 21:39:49 +0100

    Create new Event Object

--- TelegramBot/Event.ts
+++ TelegramBot/Event.ts
@@ -1,17 +1,63 @@
+export interface DanceEvents{
+    id: String,
+    title: String,
+    dances: String,
+    startDate: String,
+    city: String,
+    countryCode: String,
+    lat: Number,
+    lon: Number,
+    street: String,
+    housenumber: String,
+    startTime: String,
+    link: String,
+    chatLink: String
+};
+
 export class DanceEvent{
+    // Attributes
+    public id: String;
+    public title: String;
+    public dances: String;
+    public startDate: String;
+    public city: String;
+    public countryCode: String;
+    public lat: Number;
+    public lon: Number;
+    public street: String;
+    public housenumber: String;
+    public startTime: String;
+    public link: String;
+    public chatLink: String;
+
+    // Constructur
     constructor(
-        id: String,
+        id: String, 
         title: String, 
-        imageName: String,
-        dances: String,
-        countryCode: String,
+        dances: String, 
+        startDate: String, 
         city: String,
+        countryCode: String,
+        lat: Number,
+        lon: Number,
         street: String,
         housenumber: String,
-        startDate: String,
         startTime: String,
         link: String,
-        lat: Number,
-        lon: Number
-    ) {};
+        chatLink: String
+        ){
+            this.id = id;
+            this.title = title;
+            this.dances = dances;
+            this.startDate = startDate;
+            this.city = city;
+            this.countryCode = countryCode;
+            this.lat = lat;
+            this.lon = lon;
+            this.street = street;
+            this.housenumber = housenumber;
+            this.startTime = startTime;
+            this.link = link;
+            this.chatLink = chatLink;
+    };
 }
